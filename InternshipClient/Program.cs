f6edd7a0cfd1b1c687382802d3ded96e611e6ed6
bibashmanjusubedi <acharabibash@outlook.com>
2025-06-15 14:35:16 +0545

    Added all components for AssetCategory to Person in razor file

---
+++ InternshipClient/Program.cs
@@ -0,0 +1,14 @@
+using InternshipClient;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://bibashacharya-001-site1.anytempurl.com/")
+});
+
+await builder.Build().RunAsync();
