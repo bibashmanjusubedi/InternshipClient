3cec96af1474da407d65182a067e1bab66106765
bibashmanjusubedi <acharabibash@outlook.com>
2025-07-03 14:57:59 +0545

    Added AssetCategory Form Componenet

---
+++ InternshipClient/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿// Models/Category.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace InternshipClient.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
